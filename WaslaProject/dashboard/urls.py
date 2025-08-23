d534bf69082aeb380f0115228f9e95e4551ef077
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-08-23 18:26:49 +0300

    frontend part

---
+++ WaslaProject/dashboard/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+app_name="dashboard"
+
+
+urlpatterns = [
+    path('', views.dashboard_home_view, name="dashboard_home_view"),
+    path('add_hackathon/', views.dashboard_add_hackathon_view, name="dashboard_add_hackathon_view"),
+    path('hackathon_details/<id>/', views.dashboard_hackathon_details_view, name="dashboard_hackathon_details_view"),
+    path('team_details/<id>/', views.dashboard_team_details_view, name="dashboard_team_details_view"),
+    path('hackathons/', views.dashboard_hackathons_view, name="dashboard_hackathons_view"),
+    path('judges/', views.dashboard_judges_view, name="dashboard_judges_view"),
+    path('teams/', views.dashboard_teams_view, name="dashboard_teams_view"),
+    path('admins/', views.dashboard_admins_view, name="dashboard_admins_view"),
+    path('users/', views.dashboard_users_view, name="dashboard_users_view"),
+    path('settings/', views.dashboard_settings_view, name="dashboard_settings_view"),
+
+]
