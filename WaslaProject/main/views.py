1b6be571472e96e1c58080594f3dcc54db7c3c3b
Hassan Abdullah <hassan8abdulla@gmail.com>
2025-08-24 11:56:08 +0300

    Add home_page

--- WaslaProject/main/views.py
+++ WaslaProject/main/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_view(request:HttpRequest):
+
+
+    return render(request, 'main/home.html' )
+
+
+def base_view(request:HttpRequest):
+
+
+    return render(request, 'main/base.html' )
+
